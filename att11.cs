ba909e1200f7a94de3e30e09484f8b013a827649
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-08 20:57:16 -0300

    Add files via upload

---
+++ att11.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva um numero inteiro com 4 digitos");
+            int n1 = int.Parse(Console.ReadLine());
+
+
+            if (n1 > 0)
+            {
+
+                int n2 = n1 / 1000;
+                int n3 = (n1 / 100) % 10;
+                int n4 = (n1 / 10) % 10;
+                int n5 = (n1 % 10) ;
+             
+
+                Console.WriteLine($"{n2} {n3} {n4} {n5}");
+
+                int soma = n2 + n3 + n4 + n5;
+                Console.WriteLine("A soma dos algorismos e " + soma );
+
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Numero invalido");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
