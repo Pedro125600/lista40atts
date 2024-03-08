253408a5647c2d24615dbf17c67311b469703e73
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-08 19:30:59 -0300

    Add files via upload

---
+++ att5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um numero");
+
+            int n1 = int.Parse(Console.ReadLine());
+
+            if ( n1 % 2 == 0   )
+            {
+                Console.WriteLine("Esse numeoro e par");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Esse numeor e impar");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
